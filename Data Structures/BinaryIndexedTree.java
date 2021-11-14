6f64a09441e44d9185c7c962ac72822a2d3b4478
61izzy <89612466+61izzy@users.noreply.github.com>
2021-11-14 14:02:18 -0500

    Create BinaryIndexedTree.java

---
+++ Data Structures/BinaryIndexedTree.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Main {
+  public static int[] tree;
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int n = scanner.nextInt();
+    int[] arr = new int[n];
+    tree = new int[n + 1];
+    char q = scanner.next().toUpperCase().charAt(0);
+
+    while (q != 'E') {
+      if (q == 'U') {
+        int x = scanner.nextInt(), v = scanner.nextInt();
+        update(x, v - arr[x - 1]);
+        arr[x - 1] = v;
+      }
+      else if (q == 'S') {
+        int l = scanner.nextInt(), r = scanner.nextInt();
+        System.out.println(getSum(l, r));
+      }
+      q = scanner.next().toUpperCase().charAt(0);
+    }
+    System.out.println(Arrays.toString(arr));
+    System.out.println(Arrays.toString(tree));
+  }
+  public static void update(int x, int v) {
+    while (x < tree.length) {
+      tree[x] += v;
+      x += x & -x;
+    }
+  }
+  public static int getSum(int l, int r) {
+    return getSum(r) - getSum(l - 1);
+  }
+  public static int getSum(int x) {
+    int sum = 0;
+    for (int i = x; i > 0; i -= i & -i) {
+      sum += tree[i];
+    }
+    return sum;
+  }
+}
