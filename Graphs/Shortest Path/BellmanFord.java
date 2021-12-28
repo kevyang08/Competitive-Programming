e2014bb4d05e9b2df600630f1f6dc00b76dfedfd
61izzy <89612466+61izzy@users.noreply.github.com>
2021-12-28 18:56:03 -0500

    Update BellmanFord.java

--- Graphs/Shortest Path/BellmanFord.java
+++ Graphs/Shortest Path/BellmanFord.java
@@ -17,17 +17,18 @@ class Main {
 
     int from = scanner.nextInt(), to = scanner.nextInt();
     int dist = getDist(from, to);
-    System.out.println(dist == 0x3f3f3f3f ? "Node " + from + " is not connected to node " + to : "The shortest distance from node " + from + " to node " + to + " is " + dist);
+    System.out.println(dist == 0x3f3f3f3f ? "Node " + from + " is not connected to node " + to : dist == Integer.MIN_VALUE ? "Negative cycle detected" : "The shortest distance from node " + from + " to node " + to + " is " + dist);
   }
   public static int getDist(int from, int to) {
     int[] dist = new int[n + 1];
     Arrays.fill(dist, 0x3f3f3f3f);
     dist[from] = 0;
     
-    for (int i = 1; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
       for (Edge e : edges) {
         if (dist[e.to] > dist[e.from] + e.dist) {
           dist[e.to] = dist[e.from] + e.dist;
+          if (i == n) return Integer.MIN_VALUE;
         }
       }
     }
