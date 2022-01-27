2763b0a82ec0cc6ce217371060d91e5254b3a142
61izzy <89612466+61izzy@users.noreply.github.com>
2022-01-27 16:25:32 -0500

    Update Heapsort.java

--- Algorithms/Heapsort.java
+++ Algorithms/Heapsort.java
@@ -15,49 +15,32 @@ class Main {
     System.out.println(Arrays.toString(arr));
   }
   public static void heapsort(int[] arr) {
-    Heap heap = new Heap(arr.length);
-    for (int i : arr) heap.add(i);
-    for (int i = 0; i < arr.length; i++) arr[i] = heap.poll();
-  }
-}
-
-class Heap {
-  private int[] heap;
-  private int n = 0;
-  public Heap(int sz) {
-    heap = new int[sz + 1];
-  }
-  private void swap(int i, int j) {
-    int temp = heap[j];
-    heap[j] = heap[i];
-    heap[i] = temp;
-  }
-  private void heapUp(int i) {
-    if (i/2 > 0 && heap[i] < heap[i/2]) {
-      swap(i, i/2);
-      heapUp(i/2);
-    }
-  }
-  private void heapDown(int i) {
-    if (i * 2 <= n) {
-      if (i * 2 + 1 <= n && heap[i * 2 + 1] < heap[i * 2] && heap[i * 2 + 1] < heap[i]) {
-        swap(i, i * 2 + 1);
-        heapDown(i * 2 + 1);
+    for (int i = 0; i < arr.length; i++) {
+      int j = i;
+      while ((j + 1)/2 - 1 >= 0 && arr[j] > arr[(j + 1)/2 - 1]) {
+        swap(arr, j, (j + 1)/2 - 1);
+        j = (j + 1)/2 - 1;
       }
-      else if (heap[i * 2] < heap[i]) {
-        swap(i, i * 2);
-        heapDown(i * 2);
+    }
+    for (int i = arr.length - 1; i > 0; i--) {
+      int j = 0;
+      swap(arr, i, j);
+      while (j * 2 + 1 < i) {
+        if (j * 2 + 2 < i && arr[j * 2 + 2] > arr[j * 2 + 1] && arr[j * 2 + 2] > arr[j]) {
+          swap(arr, j, j * 2 + 2);
+          j = j * 2 + 2;
+        }
+        else if (arr[j * 2 + 1] > arr[j]) {
+          swap(arr, j, j * 2 + 1);
+          j = j * 2 + 1;
+        }
+        else break;
       }
     }
   }
-  public void add(int x) {
-    heap[++n] = x;
-    heapUp(n);
-  }
-  public int poll() {
-    int x = heap[1];
-    heap[1] = heap[n--];
-    heapDown(1);
-    return x;
+  public static void swap(int[] arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
   }
 }
