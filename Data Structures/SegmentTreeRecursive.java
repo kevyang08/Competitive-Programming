7e1e85169415fff2ea94adb0ff9057011002f1dd
61izzy <89612466+61izzy@users.noreply.github.com>
2022-02-23 20:06:09 -0500

    Update SegmentTreeRecursive.java

--- Data Structures/SegmentTreeRecursive.java
+++ Data Structures/SegmentTreeRecursive.java
@@ -29,11 +29,12 @@ class Main {
     System.out.println(Arrays.toString(st));
   }
   public static void build(int[] arr, int l, int r, int idx) {
-    if (l == r) {
+    // the current node covers range [l:r]
+    if (l == r) { // current node is leaf node
       st[idx] = arr[l];
       return;
     }
-    int m = (l + r)/2;
+    int m = (l + r)/2; // build children nodes
     build(arr, l, m, 2 * idx);
     build(arr, m + 1, r, 2 * idx + 1);
     st[idx] = st[2 * idx] + st[2 * idx + 1];
@@ -44,22 +45,22 @@ class Main {
       return;
     }
     int m = (l + r)/2;
-    if (u <= m) {
+    if (u <= m) { // node to update is within range [l:m]
       update(l, m, u, v, 2 * idx);
     }
-    else {
+    else { // node to update is within range [m + 1:r]
       update(m + 1, r, u, v, 2 * idx + 1);
     }
     st[idx] = st[2 * idx] + st[2 * idx + 1];
   }
   public static int query(int ql, int qr, int l, int r, int idx) {
-    if (l >= ql && r <= qr) {
+    if (l >= ql && r <= qr) { // if current node is fully within range, we can just return its value
       return st[idx];
     }
-    if (ql > r || qr < l) {
+    if (ql > r || qr < l) { // if current node is out of range, return nothing
       return 0;
     }
-    int m = (l + r)/2;
+    int m = (l + r)/2; // if current node is partially in range, keep traversing the tree
     return query(ql, qr, l, m, 2 * idx) + query(ql, qr, m + 1, r, 2 * idx + 1);
   }
 }
