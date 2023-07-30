8d882298e3624f50e05447b55fe8c25f77a07f94
61izzy <89612466+61izzy@users.noreply.github.com>
2023-07-29 22:11:47 -0400

    Create ccc18j2.py

---
+++ Contest Solutions/CCC/ccc18j2.py
@@ -0,0 +1,9 @@
+n = int(input())
+un = input()
+deux = input()
+eporewqriewuprqouroewuporuqeruewuorporuewproqewurewoiurweopuqrpoewiurpoweqruiwpeoqurpowerupwoeruoepwqruwe = 0
+for i in range(n):
+    if un[i] == 'C' and deux[i] == 'C':
+        eporewqriewuprqouroewuporuqeruewuorporuewproqewurewoiurweopuqrpoewiurpoweqruiwpeoqurpowerupwoeruoepwqruwe+=1
+print(eporewqriewuprqouroewuporuqeruewuorporuewproqewurewoiurweopuqrpoewiurpoweqruiwpeoqurpowerupwoeruoepwqruwe)
+# wtf is this
