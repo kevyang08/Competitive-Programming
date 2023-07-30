7c264590a810d2dc6690528ee561a7b51dcbc7ab
61izzy <89612466+61izzy@users.noreply.github.com>
2023-07-29 22:32:01 -0400

    Create mccc5j1.cpp

---
+++ Contest Solutions/CCC/mccc5j1.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int u, t;
+int main() {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    cin >> u >> t;
+    cout << u + 3 * t << "\n";
+    return 0;
+}
