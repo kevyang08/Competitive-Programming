c8e1d7a8458ca8fb2f812940788412f2a5aac34e
61izzy <89612466+61izzy@users.noreply.github.com>
2023-07-29 22:38:49 -0400

    Create ccc13j1.java

---
+++ Contest Solutions/CCC/ccc13j1.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int c1 = scanner.nextInt(), c2 = scanner.nextInt();
+        
+        System.out.println(2 * c2 - c1);
+    }
+}
