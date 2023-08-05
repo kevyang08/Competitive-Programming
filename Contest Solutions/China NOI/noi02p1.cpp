11aa9387ebe42c9e87ce2947c90aa73b28bc3016
61izzy <89612466+61izzy@users.noreply.github.com>
2023-08-05 17:21:00 -0400

    Create noi02p1.cpp

---
+++ Contest Solutions/China NOI/noi02p1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t, a, b, id[30001], pos[30001], sz[30001];
+char op;
+int find(int x) {
+    if (id[x] < 0) return x;
+    else {
+        int temp = find(id[x]);
+        if (temp != id[x]) pos[x] += pos[id[x]] - 1;
+        return (id[x] = temp);
+    }
+}
+void join(int i, int j) {
+    i = find(i); j = find(j);
+    pos[i] = sz[j] + 1;
+    sz[j] += sz[i];
+    id[i] = j;
+}
+int main() {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    fill(id, id + 30001, -1);
+    fill(pos, pos + 30001, 1);
+    fill(sz, sz + 30001, 1);
+    cin >> t;
+    while (t--) {
+        cin >> op >> a >> b;
+        if (op == 'M') join(a, b);
+        else cout << (find(a) == find(b) ? abs(pos[a] - pos[b]) - 1 : -1) << "\n";
+    }
+    return 0;
+}
