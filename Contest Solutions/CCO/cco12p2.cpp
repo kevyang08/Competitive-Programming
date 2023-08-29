e4293d02201a8a9ce6672d01dff0edc89c6cd821
Kevin Yang <89612466+61izzy@users.noreply.github.com>
2023-08-29 00:50:43 -0400

    Update cco12p2.cpp

--- Contest Solutions/CCO/cco12p2.cpp
+++ Contest Solutions/CCO/cco12p2.cpp
@@ -19,32 +19,13 @@ int main() {
         pair p = pq.top(); pq.pop();
         for (pair i : adj[p.second]) {
             if (p.first + i.second < dist[0][i.first]) {
+                dist[1][i.first] = dist[0][i.first];
                 dist[0][i.first] = p.first + i.second;
                 pq.push({dist[0][i.first], i.first});
             }
             else if (p.first + i.second > dist[0][i.first] && p.first + i.second < dist[1][i.first]) {
                 dist[1][i.first] = p.first + i.second;
                 pq.push({dist[1][i.first], i.first});
-            }
-        }
-    }
-    if (dist[1][n] == 0x3f3f3f3f) {
-        for (int i = 1; i < n; i++) {
-            if (dist[1][i] != 0x3f3f3f3f) dist[0][i] = dist[1][i];
-            pq.push({dist[0][i], i});
-        }
-        memset(dist[1], 0x3f3f3f3f, sizeof(dist[1]));
-        while (!pq.empty()) {
-            pair p = pq.top(); pq.pop();
-            for (pair i : adj[p.second]) {
-                if (p.first + i.second < dist[0][i.first]) {
-                    dist[0][i.first] = p.first + i.second;
-                    pq.push({dist[0][i.first], i.first});
-                }
-                else if (p.first + i.second > dist[0][i.first] && p.first + i.second < dist[1][i.first]) {
-                    dist[1][i.first] = p.first + i.second;
-                    pq.push({dist[1][i.first], i.first});
-                }
             }
         }
     }
