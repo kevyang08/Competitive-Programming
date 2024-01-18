e557cb7db405b66c014a9d30f0b462a5930b0eb9
Kevin Yang <89612466+61izzy@users.noreply.github.com>
2024-01-18 15:30:56 -0500

    Update 1902E.cpp

--- Contest Solutions/Codeforces/1902E.cpp
+++ Contest Solutions/Codeforces/1902E.cpp
@@ -1,49 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 struct TrieNode {
     TrieNode *child[26];
     int cnt = 0;
     TrieNode() {
         for (auto &c : child) c = nullptr;
     }
-} root;
+    ~TrieNode() {
+        for (auto &c : child) delete c;
+    }
+};
+class Trie {
+public:
+    TrieNode *root;
+    long long res;
+    Trie() {
+        root = new TrieNode();
+        res = 0;
+    }
+    ~Trie() {
+        delete root;
+    }
+    void insert(string& s) {
+        TrieNode *curr = root;
+        for (char c : s) {
+            if (curr -> child[c - 'a'] == nullptr) curr -> child[c - 'a'] = new TrieNode();
+            curr = curr -> child[c - 'a'];
+            curr -> cnt++;
+        }
+    }
+    void calc(string& s) {
+        if (root -> child[s[0] - 'a'] != nullptr) dfs(root -> child[s[0] - 'a'], s, 0);
+    }
+    int dfs(TrieNode *curr, string& s, int depth) {
+        int thing = 0;
+        if (depth + 1 < s.length() && curr -> child[s[depth + 1] - 'a'] != nullptr) thing = dfs(curr -> child[s[depth + 1] - 'a'], s, depth + 1);
+        res += 2LL * (curr -> cnt - thing) * (depth + 1);
+        return curr -> cnt;
+    }
+};
 int n;
 string s[1000000];
-long long ans = 0, res = 0;
-void insert(string& s) {
-    TrieNode *curr = &root;
-    for (char c : s) {
-        if (curr -> child[c - 'a'] == nullptr) curr -> child[c - 'a'] = new TrieNode();
-        curr = curr -> child[c - 'a'];
-        curr -> cnt++;
-    }
-}
-void calc(string& s) {
-    TrieNode *curr = root.child[s[0] - 'a'];
-    for (int i = 0; i < s.length() && curr != nullptr; i++) {
-        int thing = curr -> cnt;
-        curr = (i + 1 < s.length() ? curr -> child[s[i + 1] - 'a'] : nullptr);
-        if (curr != nullptr) thing -= curr -> cnt;
-        res += 2LL * thing * (i + 1);
-    }
-}
+long long ans = 0;
 int main() {
     cin.tie(0);
     cin.sync_with_stdio(0);
     // freopen("input.txt", "r", stdin);
     // freopen("output.txt", "w", stdout);
     cin >> n;
+    Trie trie = Trie();
     for (int i = 0; i < n; i++) {
         cin >> s[i];
         ans += s[i].length() * 2LL * n;
-        insert(s[i]);
+        trie.insert(s[i]);
     }
     for (int i = 0; i < n; i++) {
         reverse(s[i].begin(), s[i].end());
-        calc(s[i]);
+        trie.calc(s[i]);
     }
-    cout << ans - res << "\n";
+    cout << ans - trie.res << "\n";
     return 0;
 }
  
