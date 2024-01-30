d8fb04d3d5b3cfae65eeec312bf07a0620348b98
Kevin Yang <89612466+61izzy@users.noreply.github.com>
2024-01-30 13:50:48 -0500

    Create 1918A.cpp

---
+++ Contest Solutions/Codeforces/1918A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int t, n, m;
+int main() {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    cin >> t;
+    while (t--) {
+        cin >> n >> m;
+        cout << m/2 * n << "\n";
+    }
+    return 0;
+}
+ 
+/*
+tips:
+- look out for int overflow and array out of bounds
+- don't get stuck debugging a wrong solution, take time to think of a new one instead of spamming submissions
+- make sure stuestion was read correctly
+- for certain problems, solve a few cases by hand and try to make observations
+- if behind, stop looking at leaderboard and focus on solving
+*/
