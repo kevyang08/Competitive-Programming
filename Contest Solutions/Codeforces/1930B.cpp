5856987cee9ea95c53fecc4d980f24459d70eaa2
Kevin Yang <89612466+61izzy@users.noreply.github.com>
2024-02-17 14:32:33 -0500

    Create 1930B.cpp

---
+++ Contest Solutions/Codeforces/1930B.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t, n;
+int main() {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        int l = 1, r = n;
+        for (int i = 1; i <= n; i++) cout << (i%2 ? l++ : r--) << (i == n ? "\n" : " ");
+    }
+    return 0;
+}
+
+/*
+tips:
+- look out for int overflow and array out of bounds
+- don't get stuck debugging a wrong solution, take time to think of a new one instead of spamming submissions
+- make sure stuestion was read correctly
+- for certain problems, solve a few cases by hand and try to make observations
+- if behind, stop looking at leaderboard and focus on solving
+*/
