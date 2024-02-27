5bd411f8681699b4fbad22ea6b00ac865c105ac8
Kevin Yang <89612466+61izzy@users.noreply.github.com>
2024-02-26 20:22:35 -0500

    Create ioi11p2.cpp

---
+++ Contest Solutions/IOI/ioi11p2.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int ans = 0x3f3f3f3f, offc[200000];
+ll off[200000];
+vector<pair<int, int>> adj[200000];
+map<ll, int> mp[200000];
+void dfs(int i, int p, int k) {
+    for (auto j : adj[i]) {
+        if (j.first == p) continue;
+        dfs(j.first, i, k);
+        off[j.first] += j.second;
+        offc[j.first]++;
+        if (mp[j.first].size() > mp[i].size()) {
+            swap(mp[i], mp[j.first]);
+            swap(off[i], off[j.first]);
+            swap(offc[i], offc[j.first]);
+        }
+        for (auto e : mp[j.first]) {
+            if (mp[i].find(k - e.first - off[j.first] - off[i]) != mp[i].end()) {
+                ans = min(ans, e.second + mp[i][k - e.first - off[j.first] - off[i]] + offc[i] + offc[j.first]);
+            }
+        }
+        off[j.first] -= off[i];
+        offc[j.first] -= offc[i];
+        for (auto e : mp[j.first]) {
+            if (mp[i].find(e.first + off[j.first]) == mp[i].end() || mp[i][e.first + off[j.first]] > e.second + offc[j.first]) {
+                mp[i][e.first + off[j.first]] = e.second + offc[j.first];
+            }
+        }
+    }
+    if (mp[i].find(k - off[i]) != mp[i].end()) ans = min(ans, mp[i][k - off[i]] + offc[i]);
+    mp[i][-off[i]] = -offc[i];
+}
+int best_path(int N, int K, int H[][2], int L[]) {
+    for (int i = 0; i < N - 1; i++) {
+        adj[H[i][0]].push_back({H[i][1], L[i]});
+        adj[H[i][1]].push_back({H[i][0], L[i]});
+    }
+    dfs(0, N, K);
+    return (ans == 0x3f3f3f3f ? -1 : ans);
+}
+
+/*
+tips:
+- look out for int overflow and array out of bounds
+- don't get stuck debugging a wrong solution, take time to think of a new one instead of spamming submissions
+- make sure stuestion was read correctly
+- for certain problems, solve a few cases by hand and try to make observations
+- if behind, stop looking at leaderboard and focus on solving
+*/
