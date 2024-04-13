d2d01e709ab93178b553d779660b2cffb85a7f7e
Kevin Yang <89612466+61izzy@users.noreply.github.com>
2024-04-13 13:30:33 -0400

    Create 1954A.cpp

---
+++ Contest Solutions/Codeforces/1954A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t;
+int main() {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    cin >> t;
+    while (t--) {
+        int n, m, k;
+        cin >> n >> m >> k;
+        cout << (m > 1 && k < n && n - ceil((double)n/m) > k ? "YES\n" : "NO\n");
+    }
+    return 0;
+}
+
+/*
+tips:
+- look out for int overflow and array out of bounds
+- don't get stuck debugging a wrong solution, take time to think of a new one instead of spamming submissions
+- make sure stuestion was read correctly
+- for certain problems, solve a few cases by hand and try to make observations
+- if behind, stop looking at leaderboard and focus on solving
+*/
