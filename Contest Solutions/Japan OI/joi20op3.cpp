a839fcc505e9c3283e664f1da85f9c9ddfafa3f2
Kevin Yang <89612466+61izzy@users.noreply.github.com>
2024-05-16 11:04:07 -0400

    Update joi20op3.cpp

--- Contest Solutions/Japan OI/joi20op3.cpp
+++ Contest Solutions/Japan OI/joi20op3.cpp
@@ -9,19 +9,23 @@ void dfs1(int i, int p) {
         if (j == p) continue;
         dfs1(j, i);
         // if child has power generator, we can always take at least one
-        if (s[j - 1] == '1') dp[i] += max(dp[j] - 1, 1);
+        if (s[j - 1] == '1' && dp[j] == 0) dp[i]++;
+        else if (s[j - 1] == '1' && dp[j]) dp[i] += max(dp[j] - 1, 1);
         else dp[i] += dp[j];
     }
 }
-void dfs2(int i, int p, int v, bool thing) {
+void dfs2(int i, int p, int v) {
     dp[i] += v;
     for (int j : adj[i]) {
         if (j == p) continue;
         // honestly i forgot what this does but probably smth about propagating down the tree
-        if (s[i - 1] == '1' && dp[i] == dp[j] - (s[j - 1] == '1' && dp[j] > 1)) dfs2(j, i, 1, true);
-        else if (s[i - 1] == '1' && (dp[i] - dp[j] + (s[j - 1] == '1' && dp[j] > 1))) dfs2(j, i, max(1, dp[i] - dp[j] + (s[j - 1] == '1' && dp[j] > 1) - 1), true);
-        else dfs2(j, i, (thing ? max(1, dp[i] - dp[j] + (s[j - 1] == '1' && dp[j] > 1)) : dp[i] - dp[j] + (s[j - 1] == '1' && dp[j] > 1)), dp[i] - dp[j] + (s[j - 1] == '1' && dp[j] > 1) > 0 || thing);
+        if (s[i - 1] == '1' && (dp[i] <= 1 || dp[i] == dp[j] - 1)) dfs2(j, i, 1);
+        else if (s[i - 1] == '1' && dp[i] > 1 && dp[i] != dp[j] - 1) dfs2(j, i, max(1, dp[i] - dp[j] - 1));
+        else dfs2(j, i, dp[i] - dp[j]);
     }
+    // weird case where if dp[i] == 1 and s[i - 1] == 1, dp[i] remains 1
+    // only affects the ans if two adjacent nodes have power generators and no other nodes do
+    if (s[i - 1] == '1') dp[i] = (dp[i] > 2 ? dp[i] - 1 : max(dp[i], 1));
 }
 int main() {
     cin.tie(0);
@@ -36,20 +40,8 @@ int main() {
     }
     cin >> s;
     dfs1(1, 0);
-    dfs2(1, 0, 0, 0);
-    // weird case where if dp[i] == 1 and s[i - 1] == 1, dp[i] remains 1
-    for (int i = 1; i <= n; i++) if (s[i - 1] == '1') dp[i] = (dp[i] > 2 ? dp[i] - 1 : max(dp[i], 1));
-    // only affects the ans if two adjacent nodes have power generators and no other nodes do
+    dfs2(1, 0, 0);
     for (int i = 1; i <= n; i++) ans = max(ans, dp[i]);
     cout << ans << "\n";
     return 0;
 }
-
-/*
-tips:
-- look out for int overflow and array out of bounds
-- don't get stuck debugging a wrong solution, take time to think of a new one instead of spamming submissions
-- make sure stuestion was read correctly
-- for certain problems, solve a few cases by hand and try to make observations
-- if behind, stop looking at leaderboard and focus on solving
-*/
