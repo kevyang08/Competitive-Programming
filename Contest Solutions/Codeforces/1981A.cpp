a3bb09ddda07137f16e795e206310623d88770cb
Kevin Yang <89612466+61izzy@users.noreply.github.com>
2024-06-04 21:53:28 -0400

    Create 1981A.cpp

---
+++ Contest Solutions/Codeforces/1981A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t, l, r;
+int main() {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    cin >> t;
+    while (t--) {
+        cin >> l >> r;
+        int ans = 1;
+        for (int i = 2; i < 31; i++) if ((1 << i) <= r) ans = i;
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+/*
+tips:
+- look out for int overflow and array out of bounds
+- don't get stuck debugging a wrong solution, take time to think of a new one instead of spamming submissions
+- make sure stuestion was read correctly
+- for certain problems, solve a few cases by hand and try to make observations
+- if behind, stop looking at leaderboard and focus on solving
+*/
