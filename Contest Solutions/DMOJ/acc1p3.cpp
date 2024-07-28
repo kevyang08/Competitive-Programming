b7ebd61d53f4e6ad604348da39238a958a142e3e
Kevin Yang <89612466+61izzy@users.noreply.github.com>
2024-07-27 22:51:06 -0400

    don't need mod div oops

--- Contest Solutions/DMOJ/acc1p3.cpp
+++ Contest Solutions/DMOJ/acc1p3.cpp
@@ -6,29 +6,6 @@ const long long M = 998244353;
 
 int n;
 ll t, a[100001], b[100001];
-
-// bruh
-ll gcdEx(ll a, ll b, ll *x, ll *y) {
-    if (a%b == 0) {
-        *x = 0;
-        *y = 1;
-        return b;
-    }
-    ll x1, y1, gcd = gcdEx(b, a%b, &x1, &y1);
-    *x = y1;
-    *y = x1 - (a/b) * y1;
-    return gcd;
-}
-ll modInv(ll a, ll m) {
-    ll x, y;
-    gcdEx(a, m, &x, &y);
-    return (x%m + m)%m;
-}
-ll modDiv(ll a, ll b, ll m) {
-    ll c = modInv(b, m);
-    return (c * (a%m))%m;
-}
-
 int main() {
     cin.tie(0);
     cin.sync_with_stdio(0);
@@ -44,7 +21,7 @@ int main() {
             if (a[i] <= m) continue;
             ll temp = (a[i] - m)/b[i] + 1;
             if (a[i] - b[i] * (temp - 1) == m) --temp, ++bruh;
-            res = (res + ((a[i]%M) * (temp%M) - modDiv(((temp - 1)%M) * (temp%M), 2, M) * (b[i]%M) + M * M)%M + M * M)%M;
+            res = (res + ((a[i]%M) * (temp%M) - (((temp - 1)%M * (temp%M)/2)%M * (b[i]%M))%M)%M + M * M)%M;
             c += temp;
             if (c > 2 * t) break;
         }
